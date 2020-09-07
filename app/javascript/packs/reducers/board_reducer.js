b5044eef2d3f16129da72136b3d060e566cc5c6f
Nikolay Ponomarev <itsnikolay@gmail.com>
2020-09-07 16:40:41 +0300

    winner logic

--- app/javascript/packs/reducers/board_reducer.js
+++ app/javascript/packs/reducers/board_reducer.js
@@ -1,7 +1,19 @@
 const INITIAL_STATE = {
   board: [0, 1, 2, 3, 4, 5, 6, 7, 8],
   round: 0,
-  currentPlayer: 0
+  currentPlayer: 0,
+  winner: null
+};
+
+const winning = (board, player) => {
+  return (board[0] == player && board[1] == player && board[2] == player) ||
+    (board[3] == player && board[4] == player && board[5] == player) ||
+    (board[6] == player && board[7] == player && board[8] == player) ||
+    (board[0] == player && board[3] == player && board[6] == player) ||
+    (board[1] == player && board[4] == player && board[7] == player) ||
+    (board[2] == player && board[5] == player && board[8] == player) ||
+    (board[0] == player && board[4] == player && board[8] == player) ||
+    (board[2] == player && board[4] == player && board[6] == player)
 };
 
 const reducer = (state = INITIAL_STATE, action) => {
@@ -9,11 +21,13 @@ const reducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case 'move':
       const player = state.currentPlayer == 0 ? 'P1' : 'P2'
+      const board = state.board.map((el, index) => (index == action.index ? player : el));
       return {
         ...state,
-        ...{ board: state.board.map((el, index) => (index == action.index ? player : el)) },
+        ...{ board: board },
         ...{ round: state.round + 1 },
-        ...{ currentPlayer: state.currentPlayer == 0 ? 1 : 0 }
+        ...{ currentPlayer: state.currentPlayer == 0 ? 1 : 0 },
+        ...{ winner: winning(board, player) ? player : undefined }
       };
     default: return state;
   }
