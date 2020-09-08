8805c49b48cbca47cf8fea0cde2c665b9ac70414
Nikolay Ponomarev <itsnikolay@gmail.com>
2020-09-08 11:20:53 +0300

    extract logic to server

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,10 +1,6 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
-    @room = Room.find_or_create_by!(name: 'Main') do |room|
-      room.update!(current_turn_user_id: current_user.id)
-    end
-    subscribed_user_ids = @room.subscribed_user_ids.concat([current_user.id]).uniq
-    @room.update!(subscribed_user_ids: subscribed_user_ids)
+    @room = Rooms::RoomInterface.new(current_user, nil).build
     stream_for current_user
     broadcast_subscribers
     broadcast_to(current_user, current_user_id: current_user.id, current_user_name: current_user.name)
@@ -13,17 +9,13 @@ class RoomChannel < ApplicationCable::Channel
   def unsubscribed; end
 
   def move(data)
-    @room.reload
-    cells = @room.cells.to_a
     index = data['index'].to_i
-    cells[index] = current_user.id
-
-    @room.update!(cells: cells, current_turn_user_id: current_turn_user_id, winner: winning)
+    @room = Rooms::RoomInterface.new(current_user, @room).move(index)
     broadcast_subscribers
   end
 
   def new_game
-    @room.update!(cells: Array.new(3 * 3), winner: nil)
+    @room = Rooms::RoomInterface.new(current_user, @room).new_game
     broadcast_subscribers
   end
 
@@ -33,26 +25,5 @@ class RoomChannel < ApplicationCable::Channel
     User.where(id: @room.subscribed_user_ids).each do |user|
       broadcast_to(user, room: @room)
     end
-  end
-
-  def winning
-    board = @room.cells
-    player = current_user.id
-
-    return unless (board[0] == player && board[1] == player && board[2] == player) ||
-                  (board[3] == player && board[4] == player && board[5] == player) ||
-                  (board[6] == player && board[7] == player && board[8] == player) ||
-                  (board[0] == player && board[3] == player && board[6] == player) ||
-                  (board[1] == player && board[4] == player && board[7] == player) ||
-                  (board[2] == player && board[5] == player && board[8] == player) ||
-                  (board[0] == player && board[4] == player && board[8] == player) ||
-                  (board[2] == player && board[4] == player && board[6] == player)
-
-    player
-  end
-
-  def current_turn_user_id
-    index = @room.subscribed_user_ids.index(current_user.id)
-    @room.subscribed_user_ids[index - 1]
   end
 end
---
+++ app/services/rooms/room_interface.rb
@@ -0,0 +1,73 @@
+module Rooms
+  # Rooms::RoomInterface
+  class RoomInterface
+    LINES = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ].freeze
+
+    attr_reader :current_user, :room
+
+    delegate :cells, to: :room
+
+    def initialize(current_user, room = nil)
+      @current_user = current_user
+      @room = room
+    end
+
+    def build
+      @room = Room.find_or_create_by!(name: 'Main') do |room|
+        room.update!(current_turn_user_id: current_user.id)
+      end
+      room.update!(subscribed_user_ids: subscribed_user_ids)
+      room
+    end
+
+    def move(index)
+      room.reload
+      cells = @room.cells.to_a
+      cells[index.to_i] = current_user.id
+
+      room.update!(cells: cells, current_turn_user_id: current_turn_user_id, winner: winning)
+      room
+    end
+
+    def new_game
+      room.update!(cells: Array.new(3 * 3), winner: nil)
+      room
+    end
+
+    private
+
+    def winning
+      return unless LINES.find do |first, second, third|
+        [
+          cells[first]  == user_id,
+          cells[second] == user_id,
+          cells[third]  == user_id
+        ].all?
+      end
+
+      user_id
+    end
+
+    def current_turn_user_id
+      index = room.subscribed_user_ids.index(current_user.id)
+      room.subscribed_user_ids[index - 1]
+    end
+
+    def subscribed_user_ids
+      room.subscribed_user_ids.concat([current_user.id]).uniq
+    end
+
+    def user_id
+      current_user.id
+    end
+  end
+end
